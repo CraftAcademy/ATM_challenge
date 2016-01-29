893cce5155f67b23ba5505a52e393c671ed65761
tochman <thomas@nocebo.se>
2016-01-29 10:26:35 +0100

    some re-factoring of methods

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,16 +8,26 @@ describe Account do
     expect(subject_2.holder).to eq person
   end
 
+  it 'expect new account exp_date to be +5 years' do
+    date_5_yrs = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject_2.exp_date).to eq date_5_yrs
+  end
+
+  it 'expect new account pin to be 4 digits' do
+    pin_digits = Math.log10(subject_2.pin).to_i + 1
+    expect(pin_digits).to eq 4
+  end
+
   it 'expect new account initial balance to be 100' do
     expect(subject_2.balance).to eq 100
   end
 
-  it 'add money to person account ' do
+  it 'expect to allow a deposit to be made' do
     subject.deposit(500)
     expect(subject.balance).to eq 500
   end
 
-  it 'deduct money when atm withdraw from balance' do
+  it 'expect to deduct money when atm withdraw from balance' do
     subject.atm_withdraw(40)
     expect(subject.balance).to eq -40
   end
